a3618a77e2af8212d39507b3cd62b2afe24ad0d2
Sara Kubik <27822918+sarakubik@users.noreply.github.com>
2021-12-29 12:44:59 -0600

    Add files via upload

---
+++ creating new table.sql
@@ -0,0 +1,11 @@
+-- tutorial from --renames columns to be more easily understandable
+
+-- create a new database and tables
+create table Customer2
+(
+
+	FirstName varchar(50),
+	LastName varchar(50),
+	Age int
+
+)
---
+++ inserting data into a table.sql
@@ -0,0 +1,16 @@
+-- insert data
+
+insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Joey', 'Blue', 40);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Haey', 'Jones', 52);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Dude', 'Schemit', 45);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Sally', 'White', 48);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Cal', 'Lognerd', 55);
